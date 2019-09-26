5c991285ddd5d1f7b45d18cd3e52e01412c82b17
Ilya <stemasoff1@yandex.ru>
2019-09-26 12:51:12 +0300

    Added second lab

---
+++ lab2_RC4/RC4.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+from operator import xor
+
+def s_block_generate(text_or_cipher, key, n):
+    '''
+    :param text_or_cipher: Текст или шифр, на основе которого нужно сгенерировать s_block
+    :param key: Пользовательский ключ
+    :param n: Количество бит
+    :return: Шифрограмма s_block(list int)
+    '''
+
+    #   Инициализация KSA(key-scheduling algorithm)
+    #   s - последовательность битов
+    #   temp_key - переменная, длинной в s, которая состоит из ключа key
+    s = [i for i in range(2**n)]
+    temp_key = [key[i % len(key)] for i in s]
+
+    #   Перемешиваем массив, путем перестановок, определяемых ASCII-кодом ключа
+    j = 0
+    for i in range(2**n):
+        j = (j + s[i] + ord(temp_key[i])) % 2**n
+        s[i], s[j] = s[j], s[i]
+
+    #   Генерация псевдослучайного кода PRGA(pseudo-random generation algorithm)
+    #   s_block - переменная, которая будет содержать перестановку 2^n возможных значений слова
+    s_block = list()
+    i, j = 0, 0
+    length_text = len(text_or_cipher)
+    '''
+    Генератор ключевого потока RC4 переставляет значения, хранящиеся в s,
+    и каждый раз выбирает различное значение из s в качестве результата. В одном цикле определяется
+    одно n-битное слово из ключевого потока.
+    '''
+    while length_text > 0:
+        i = (i + 1) % 2**n
+        j = (j + s[i]) % 2**n
+        s[i], s[j] = s[j], s[i]
+        s_block.append(s[(s[i] + s[j]) % 2**n])
+        length_text -= 1
+    return s_block
+
+def encode(text, s_block):
+    '''
+    Шифрование
+    :param Text: Текст, который нужно зашифровать
+    :param s_block: Шифрограмма
+    :return: Шифр(list int)
+    '''
+
+    # Сложение по модулю 2 i-го элемента шифрограммы и ascii-кода символа в тексте.
+    cipher = [xor(ord(text[i]), s_block[i]) for i in range(len(text))]
+    return cipher
+
+
+def decode(cipher, s_block):
+    '''
+    Расшифровка
+    :param cipher: Шифр
+    :param s_block: Шифрограмма
+    :return: Расшифрованный текст(str)
+    '''
+    decoded_text = str()
+
+    abc = [xor(int(cipher[i]), int(s_block[i])) for i in range(len(cipher))]
+    for i in abc:
+        decoded_text = decoded_text + chr(i)
+    return decoded_text
---
+++ lab2_RC4/main.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import RC4
+
+n = int(input('Введите длину ключа: '))
+key = input('Введите ключ: ')
+action = input('Выберите действие. Зашифровать файл - 1. Для расшифровки введите любой символ: ')
+
+if action == '1':
+
+    with open('text.txt', 'r', encoding='utf-8') as inpt_data:
+        # Открываем исходный файл и записываем данные из него в переменную data.
+        data = inpt_data.read()
+
+    # Генерируем шифрограмму и шифруем исходные данные
+    s_block = RC4.s_block_generate(data, key, n)
+    encoded_data = RC4.encode(data, s_block)
+
+    with open('encoded.txt', 'w', encoding='utf-8') as out_data:
+        # Выводим шифр в файл encoded.txt
+        for element in encoded_data:
+            out_data.write(str(element) + ' ')
+else:
+    with open('encoded.txt', 'r', encoding='utf-8') as inpt_data:
+        # Открываем файл с шифром и записываем данные из него в переменную data.
+        data = inpt_data.read().split()
+
+    # Повторно генерируем шифрограмму и расшифровываем данные.
+    s_block = RC4.s_block_generate(data, key, n)
+    decoded_data = RC4.decode(data, s_block)
+
+    with open('decoded.txt', 'w', encoding='utf-8') as out_data:
+        # Записываем результат в decoded.txt
+        for element in decoded_data:
+            out_data.write(element)
