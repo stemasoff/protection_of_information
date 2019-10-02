972edba1949497fe3bd969ae7116e767271f02a2
stemasoff <stemasoff1@yandex.ru>
2019-10-02 21:48:11 +0300

    fixed lab3

--- lab3_MHKC/main.py
+++ lab3_MHKC/main.py
@@ -1,25 +1,24 @@
 import random
-
 # Пользовательские данные
-word = 'Hello, World!'
-
+word = 'Hello World!'
 # Супервозрастающая последовательность
-w = (2, 7, 11, 21, 42, 89, 180, 354)
+w = (2, 7, 11, 21, 42, 89, 180, 390)
 
-# Число, превосходящее сумму элементов последовательности w
-q = sum(w) + random.choice(w)
+# Простое число, превосходящее сумму элементов последовательности w
+q = 881
 
 # Случайное число из интервала [1,q)
 r = random.randrange(1, q)
 
 # Открытый ключ
 open_key = tuple([(i * r) % q for i in w])
+print(open_key)
 
-def encoding():
+def encoding(word, open_key):
     # Переводим слово в двоичный код
     # Если двоичный код по длине меньше, чем длина ключа, то дописываем 0 перед двоичным словом.
     bit_word = [('0' * (len(open_key) - len(format(ord(i), 'b')))) + format(ord(i), 'b') for i in word]
-    print(bit_word)
+
     # Создание шифра, путем перемножения j-ых элементов i-ого слова и j-ого жлемента ключа
     cipher = list()
     for i in range(len(bit_word)):
@@ -29,21 +28,39 @@ def encoding():
         cipher.append(sum(amount))
     return cipher
 
-def decoding(cipher):
+def decoding(cipher, w, r, q):
     # Мультипликативное обратное r по модулю q
     r_inverse = pow(r, q-2, q)
+    decoded_dig = list()
+    decoded_text = str()
 
-    for element in cipher:
-        temp = (element * r_inverse) % q
-        
-        while temp > min(w):
-            temp
+    for i in range(len(cipher)):
+        temp = (cipher[i] * r_inverse) % q
+        temp_w = list(w)
+
+        decoded_dig.append('0' * len(w))
+
+        # Раскладываем временную переменную по делителям
+        while temp >= min(w):
+            temp_w.append(temp)
+            temp_w.sort()
+
+            if temp_w.count(temp) > 1:
+                decoded_dig[i] = decoded_dig[i][:temp_w.index(temp)] + '1' + decoded_dig[i][temp_w.index(temp)+1:]
+                temp = temp - temp_w[temp_w.index(temp)]
+            else:
+                decoded_dig[i] = decoded_dig[i][:temp_w.index(temp)-1] + '1' + decoded_dig[i][temp_w.index(temp):]
+                temp = temp - temp_w[temp_w.index(temp) - 1]
+        decoded_text = decoded_text + chr(int(decoded_dig[i],2))
 
 
+    return decoded_text
 
-print(w)
-print(q)
-print(r)
-print(open_key)
-print()
-print(encoding())
+print('Исходный текст:', word)
+print('Ваш ключ:\nR:',r ,'\nw:', w, '\nq:', q)
+
+cipher = encoding(word, open_key)
+decoded_text = decoding(cipher, w, r, q)
+
+print('Зашифрованный текст:', cipher)
+print('Расшифрованный текст:', decoded_text)
